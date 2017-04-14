98b7153d82c0030faa1e10e60139eec453c9f382
wuxiucong <wuxiucong@yitianxinda.com>
2017-04-14 10:23:51 +0800

    2017-04-14上午

---
+++ dreammarket-user/src/main/java/com/excellent/dmu/ui/fm/home/HomeFM2.java
@@ -0,0 +1,27 @@
+package com.excellent.dmu.ui.fm.home;
+
+
+import com.excellent.dmu.R;
+import com.excellent.dmu.base.BaseFragment;
+
+/**
+ * Created by apple on 2017/3/29.
+ */
+
+public class HomeFM2 extends BaseFragment {
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.fm_home_2;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
---
+++ dreammarket-user/src/main/java/com/excellent/dmu/ui/fm/home/HomeFM4.java
@@ -0,0 +1,27 @@
+package com.excellent.dmu.ui.fm.home;
+
+
+import com.excellent.dmu.R;
+import com.excellent.dmu.base.BaseFragment;
+
+/**
+ * Created by apple on 2017/3/29.
+ */
+
+public class HomeFM4 extends BaseFragment {
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.fm_home_1;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
